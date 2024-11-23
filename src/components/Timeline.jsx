614747a56f188bc49e8c8a46672d44562f504869
bilaal5279 <bilaal5279@gmail.com>
2024-11-23 15:34:03 +0000

    Changed Timeline

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -47,40 +47,41 @@ const Timeline = () => {
   iconStyle={{ background: '#000', color: '#fff' }}
   icon={<FontAwesomeIcon icon={faHeart} />} // use font awesome icon
 >
-  <h3 className="vertical-timeline-element-title"> VyroAi</h3>
+<h3 className="vertical-timeline-element-title">CareerStudio</h3>
   <h4 className="vertical-timeline-element-subtitle">Personal Project</h4>
   <p>
-  As part of my ongoing commitment to innovation and education, I recently designed a sophisticated web application that 
-  leverages GPT technology to help students learn and test their own abilities on a particular topic. This app represents a significant
-  breakthrough in my knowledge, and I am excited to continue exploring new ways to enhance the learning experience for students around the world.
+  Developed a Resume Builder and Cover Letter Creator tailored to specific job applications using
+OpenAI's API. This project leverages AI to generate personalized and professional resumes and
+cover letters, streamlining the application process for users. Designed with a user-friendly interface
+and robust customization options to meet diverse job requirements
   </p>
-  <a href="https://www.vyroai.com/" target="_blank" rel="noreferrer" className="inline-block mr-2 cursor-pointer underline" >Visit VyroAi Website</a> 
-  <a href="https://github.com/VyroAI/vyroai-webapp" target="_blank" rel="noreferrer" className="inline-block mr-2 cursor-pointer underline">View GitHub Code (Private)</a> 
-</VerticalTimelineElement>
-<VerticalTimelineElement
-  className="vertical-timeline-element--work"
-  contentStyle={{ background: '#000', color: '#fff' }}
-  contentArrowStyle={{ borderRight: '7px solid  #000' }}
-  date="April 2023 - present"
-  iconStyle={{ background: '#000', color: '#fff' }}
-  icon={<FontAwesomeIcon icon={faLaptopCode} />} // use font awesome icon
->
-  <h3 className="vertical-timeline-element-title">Working on ***********</h3>
-  <h4 className="vertical-timeline-element-subtitle">Proffesional Project</h4>
-  <p>
-  As an intern at JXML INC, I am currently working on the engineering of a sophisticated web application. 
-  My responsibilities include designing and implementing key features of the application, 
-  as well as troubleshooting any issues that arise during the development process. 
-  I am utilizing my knowledge of programming languages such as HTML, CSS, JavaScript and React to build an intuitive and 
-  user-friendly interface that meets the needs of our target audience  </p>
   <a href="#" target="_blank" rel="noreferrer" className="inline-block mr-2 cursor-pointer underline" >Visit  Website (N/A)</a> 
-  <a href="" target="_blank" rel="noreferrer" className="inline-block mr-2 cursor-pointer underline">View GitHub Code (N/A)</a> 
+  <a href="https://github.com/bilaal5279/CareerStudio" target="_blank" rel="noreferrer" className="inline-block mr-2 cursor-pointer underline">View GitHub Code</a> 
 </VerticalTimelineElement>
 <VerticalTimelineElement
   className="vertical-timeline-element--work"
   contentStyle={{ background: '#000', color: '#fff' }}
   contentArrowStyle={{ borderRight: '7px solid  #000' }}
-  date="May 2023 - present"
+  date="Feb 2023 - Jun 2023"
+  iconStyle={{ background: '#000', color: '#fff' }}
+  icon={<FontAwesomeIcon icon={faFire} />} // use font awesome icon
+>
+  <h3 className="vertical-timeline-element-title">Green Air</h3>
+  <h4 className="vertical-timeline-element-subtitle">Personal Project</h4>
+  <p>
+  A front-end focused project utilizing React, Tailwind CSS, and Vite. Designed and implemented an
+intuitive and responsive user interface, emphasizing clean design principles and efficient
+development practices.
+
+  </p>
+  <a href="https://greenairconditioning.netlify.app/" target="_blank" rel="noreferrer" className="inline-block mr-2 cursor-pointer underline" >Visit  Website </a> 
+  <a href="https://github.com/bilaal5279/GreenAir" target="_blank" rel="noreferrer" className="inline-block mr-2 cursor-pointer underline">View GitHub Code</a> 
+</VerticalTimelineElement>
+<VerticalTimelineElement
+  className="vertical-timeline-element--work"
+  contentStyle={{ background: '#000', color: '#fff' }}
+  contentArrowStyle={{ borderRight: '7px solid  #000' }}
+  date="Feb 2023 - Jun 2023"
   iconStyle={{ background: '#000', color: '#fff' }}
   icon={<FontAwesomeIcon icon={faFire} />} // use font awesome icon
 >
